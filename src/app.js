ec9c7b7d6e8a238be7284abac7182d2789fefefd
iylus <ali.uppal@zigron.com>
2021-09-21 17:10:58 +0500

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const getGeocode = require('./geocode');
 const getWeather = require('./weather');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // define paths for express config
 const publicDirecytoryPath = path.join(__dirname, '../public');
@@ -67,6 +68,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log(`server is up on port: 3000`);
+app.listen(port, () => {
+    console.log(`server is up on port: ${port}`);
 });
